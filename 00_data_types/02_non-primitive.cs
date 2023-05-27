df5eaee411d117c0c9bfafa06761b37b93cecd57
Preshfile <preshcharlies2014@gmail.com>
2023-05-27 16:12:47 +0100

    understanding string

---
+++ 00_data_types/02_non-primitive.cs
@@ -0,0 +1,7 @@
+//declear non-prmitive type with <myFirstName>, <mySurname>, and a concatenation of both as <myFullName>
+
+string myFirstName = "Precious";
+string mySurname = "Oranye";
+string myFullName = myFirstName + " " + mySurname;
+
+Console.WriteLine("My full name is " + myFullName);
