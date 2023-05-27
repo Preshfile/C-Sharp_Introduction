a281d63745fd1be41b644919212e9a90359e95a4
Preshfile <preshcharlies2014@gmail.com>
2023-05-27 14:03:49 +0100

    understanding primitive type

---
+++ data_types/primitive1.cs
@@ -0,0 +1,8 @@
+//Write a program that displays the following message:"My name is <myFirstName>, I am <age> years old. I am an <grade> grade student."
+
+int age = 30;
+string firstName = "Ifenna";
+char grade = 'A';
+
+Console.WriteLine("My name is " + firstName + ", I am " +  age + " years old. I am an " + grade + " grade student.");
+
