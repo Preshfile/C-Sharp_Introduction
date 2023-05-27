9f79aa9b6e188f1c374fd01dffa1b398f437fa73
Preshfile <preshcharlies2014@gmail.com>
2023-05-27 15:02:02 +0100

    understanding primitive type 1

---
+++ 00_data_types/00_Message.cs
@@ -0,0 +1,6 @@
+int age = 30;
+string firstName = "Ifenna";
+char grade = 'A';
+
+Console.WriteLine("My name is " + firstName + ", I am " +  age + " years old. I am an " + grade + " grde student.");     
+
