35ed9bb91af764282a2215eb922d6e2b8d3952a7
Preshfile <preshcharlies2014@gmail.com>
2023-05-28 16:05:30 +0100

    substring

---
+++ 00_data_types/07_substring.cs
@@ -0,0 +1,6 @@
+//Get the substring "Precious" from the giving string
+
+string introduceSelf = "My name is Precious";
+string getSubString = introduceSelf.Substring(11);
+
+Console.WriteLine(getSubString);
