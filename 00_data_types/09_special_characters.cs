c6ac298eaf75eb80cb33df5d3beebf3e28ebf313
Preshfile <preshcharlies2014@gmail.com>
2023-05-28 16:38:54 +0100

    Using special characters

---
+++ 00_data_types/09_special_characters.cs
@@ -0,0 +1,20 @@
+//initiate the following special characters: slash, single and double quotes, new line, tab and backspace
+
+//Using slash
+string adeScore = "80\\100";
+Console.WriteLine($"Ade scored {adeScore}\n\n");
+
+
+//using single quotes
+string adeQuote = "\'It's not a faith in technology. It's faith in people.\' \n\n";
+Console.WriteLine($"Popular quote: {adeQuote} ");
+
+
+//using double quotes
+string adeQuote_ = "\"Technology is best when it brings people together.\"";
+Console.WriteLine($"Popular quote: {adeQuote_}\n\n");
+
+
+//initiating a new line, tab and backspace
+string adeNewLine = "This is a new line.\nThis is tab\t. After tab.\n This is backspace\b";
+Console.WriteLine(adeNewLine);
