c887ad61436fba04790e6d215590a17c16a65637
Preshfile <preshcharlies2014@gmail.com>
2023-05-28 16:12:24 +0100

    Start and end index substring

---
+++ 00_data_types/08_substring.cs
@@ -0,0 +1,6 @@
+//Get "My name" as a specified length of the string
+
+string introduceSelf = "My name is Precious";
+string getTheSubString = introduceSelf.Substring(0, 7);
+
+Console.WriteLine(getTheSubString);
