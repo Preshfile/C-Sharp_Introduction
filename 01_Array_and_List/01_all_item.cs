6ef7ba646cb951d7940560aa83ddd8c00ceb2907
Preshfile <preshcharlies2014@gmial.com>
2023-06-01 16:16:13 +0100

    understanding array

---
+++ 01_Array_and_List/01_all_item.cs
@@ -0,0 +1,8 @@
+//Declare an array of type short and assign value to it. Print out all the items in the array.
+
+short[] victoriaScores;
+victoriaScores = new short[] { 1, 2, 3, 4 };
+for (int i = 0; i < victoriaScores.Length; i++)
+{
+	    Console.Write(victoriaScores[i]);
+}
