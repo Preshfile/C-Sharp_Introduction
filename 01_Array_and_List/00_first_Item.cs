53566e89b38423169b3787d0289787109ac70faa
Preshfile <preshcharlies2014@gmial.com>
2023-06-01 16:12:29 +0100

    understanding array

---
+++ 01_Array_and_List/00_first_Item.cs
@@ -0,0 +1,6 @@
+//Declare an array of type short and assign value to it. Print out the first item in the array.
+
+short[] numberCount;
+numberCount = new short[] { 1, 2, 3, 4};
+
+Console.Write(numberCount[0]);
